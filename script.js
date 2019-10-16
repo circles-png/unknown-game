47085f953d2b51bf20e1e3eac30191120461adde
ML-10 <matthew.li10@education.nsw.gov.au>
2019-10-16 21:13:34 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-b1 = $('#button1')
-b2 = $('#button2')
-b3 = $('#button3')
-b1.click(function() {
-  b1.remove()
-  b2.remove()
-  b3.remove()
-});
-b2.click(function() {
-  b1.remove()
-  b2.remove()
-  b3.remove()
-});
-b3.click(function() {
-  b1.remove()
-  b2.remove()
-  b3.remove()
-});
+window.onload = function(){
+    b1 = document.getElementById('button1');
+    b2 = document.getElementById('button2');
+    b3 = document.getElementById('button3');
+    b1.onclick = function(){
+        b1.remove();
+        b2.remove();
+        b3.remove();
+    };
+    b2.onclick = function(){
+        b1.remove();
+        b2.remove();
+        b3.remove();
+    };
+    b3.onclick = function(){
+        b1.remove();
+        b2.remove();
+        b3.remove();
+    };
+};
