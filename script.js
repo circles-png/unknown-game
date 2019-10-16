63bfba68e61b3f51e1c38ba3cd0d7d11a43e69bb
ML-10 <matthew.li10@education.nsw.gov.au>
2019-10-16 20:35:14 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-
+b1 = $('#button1')
+b2 = $('#button2')
+b3 = $('#button3')
+b1.click(function() {
+  b1.remove()
+  b2.remove()
+  b3.remove()
+});
+b2.click(function() {
+  b1.remove()
+  b2.remove()
+  b3.remove()
+});
+b3.click(function() {
+  b1.remove()
+  b2.remove()
+  b3.remove()
+});
