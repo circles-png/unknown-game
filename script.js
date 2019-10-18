fb89322e26b15df6ac18f0320f4b7cda5f17b442
ML-10 <matthew.li10@education.nsw.gov.au>
2019-10-18 17:49:34 +1100

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,15 @@ window.onload = function(){
     b1 = document.getElementById('button1');
     b2 = document.getElementById('button2');
     b3 = document.getElementById('button3');
-    b1.onclick = function(){
+    game = document.getElementById('game');
+    function start(){
         b1.remove();
         b2.remove();
         b3.remove();
-    };
-    b2.onclick = function(){
-        b1.remove();
-        b2.remove();
-        b3.remove();
-    };
-    b3.onclick = function(){
-        b1.remove();
-        b2.remove();
-        b3.remove();
-    };
+        game.style.transition = backgroundColor 3s;
+        game.style.backgroundColor = white;      
+    }
+    b1.onclick = start();
+    b2.onclick = start();
+    b3.onclick = start();  
 };
